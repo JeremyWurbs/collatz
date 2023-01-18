2c3fd9c45a1dc55a0cd22844b1f9aab0102d1f83
Jeremy <jdwurbs@gmail.com>
2023-01-19 05:46:25 +0900

    Initial collatz script

---
+++ main.py
@@ -0,0 +1,43 @@
+from dsplot.graph import Graph
+
+
+def compute_graph(N):
+    graph = dict()
+    for n in range(1, N + 1):
+        graph = add_branch(graph, n)
+    return graph
+
+
+def add_branch(graph, n):
+    while n not in graph:
+        if n % 2 == 0:
+            d = int(n / 2)
+        else:
+            d = int(3 * n + 1)
+        graph[n] = [d]
+        n = d
+    return graph
+
+
+def path(graph, n):
+    p = graph[n]
+    while p[-1] > 1:
+        p.append(graph[p[-1]][0])
+    return p
+
+
+def display_graph(graph):
+    dsgraph = Graph(graph, directed=True)
+    dsgraph.plot(orientation='TB')
+
+
+def main(N):
+    graph = compute_graph(N)
+    for k in range(1, N+1):
+        p = path(graph, k)
+        print(f'{k} ({len(p)}): {path(graph, k)}')
+    display_graph(graph)
+
+
+if __name__ == '__main__':
+    main(1000)
